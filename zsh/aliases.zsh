08b4c8ddeb0bdf2f1b6fccdc62ca4ec0f5494338
Breno Teodoro <brenodt@Brenos-Mac-mini.local>
2023-03-30 20:07:00 -0300

    updating dotfiles configuration to something more useful, since i've been using things in different ways

---
+++ zsh/aliases.zsh
@@ -0,0 +1,25 @@
+# ALIASES -------------------------------------------------------------
+alias vi='nvim'
+alias vim='nvim'
+
+alias c='clear'
+alias so='source ~/.zshrc'
+
+alias l='exa -lah'
+alias ls=exa
+alias sl=exa
+
+# GIT ALIASES ---------------------------------------------------------
+alias gc='git commit'
+alias gco='git checkout'
+alias ga='git add'
+alias grp='git remote prune origin'
+alias gff='grp && g pull --ff-only'
+alias gmc='git merge --continue'
+alias grc='git rebase --continue'
+
+# FUNCTIONS -----------------------------------------------------------
+function take {
+  mkdir -p $1
+  cd $1
+}
